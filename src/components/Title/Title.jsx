647fed5a333db5a946232c1d36c0f7dd7d97cff2
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-10-13 01:03:20 +0500

    added logo

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,37 +1,43 @@
 import React from "react";
 import { Box, Typography, Divider } from "@mui/material";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 const Title = () => {
   return (
     <>
-    <Box style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      alignContent: 'center',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop:"20px"
-    }}>
-     <img src="/assests/logo.jpg" alt="image" border="0"
-     width="200" height="200" 
-     
-     />
-     
-
-    </Box>
+      <Box
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignContent: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        <img
+          src="/assests/logo.jpg"
+          alt="image"
+          border="0"
+          width="200"
+          height="200"
+        />
+      </Box>
       <Box p={4}>
-        <Link to="/" style={{
-          color: 'black',
-          textDecoration: 'none'
-        }}>
-        <Typography
-          variant="h4"
-          component="h4"
-          align="center"
-          fontWeight="bold"
+        <Link
+          to="/"
+          style={{
+            color: "black",
+            textDecoration: "none",
+          }}
         >
-          Durood & Zikr Counter
-        </Typography>
+          <Typography
+            variant="h4"
+            component="h4"
+            align="center"
+            fontWeight="bold"
+          >
+            Durood & Zikr Counter
+          </Typography>
         </Link>
         <Divider />
       </Box>
